ee21cfa1e84d5c6d9e04dd626926e34a2181fa75
Abbath <pitongogi@gmail.com>
2024-09-06 17:21:13 +0200

    Day 3. Part 2.

--- src/main.rs
+++ src/main.rs
@@ -122,6 +122,7 @@ fn day_03() {
         start: (i64, i64),
         dir: Dir,
         len: i64,
+        clen: i64,
     }
 
     impl Segment {
@@ -142,7 +143,21 @@ fn day_03() {
             || y2 <= y3 && y1 >= y3 && x4 <= x1 && x3 >= x1
     }
 
-    fn intersect(s1: &Segment, s2: &Segment) -> Option<(i64, i64)> {
+    fn check2(s1: &Segment, s2: &Segment, x: i64, y: i64) -> i64 {
+        match (s1.dir, s2.dir) {
+            (Dir::U, Dir::L) => (y - s1.start.1) + (s2.start.0 - x),
+            (Dir::U, Dir::R) => (y - s1.start.1) + (x - s2.start.0),
+            (Dir::D, Dir::L) => (s1.start.1 - y) + (s2.start.0 - x),
+            (Dir::D, Dir::R) => (s1.start.1 - y) + (x - s2.start.0),
+            (Dir::L, Dir::U) => check2(s2, s1, x, y),
+            (Dir::R, Dir::U) => check2(s2, s1, x, y),
+            (Dir::L, Dir::D) => check2(s2, s1, x, y),
+            (Dir::R, Dir::D) => check2(s2, s1, x, y),
+            _ => 0,
+        }
+    }
+
+    fn intersect(s1: &Segment, s2: &Segment) -> Option<(i64, i64, i64)> {
         match (s1.dir, s2.dir) {
             (Dir::U, Dir::L) | (Dir::U, Dir::R) | (Dir::D, Dir::L) | (Dir::D, Dir::R) => {
                 let e1 = s1.end();
@@ -150,7 +165,11 @@ fn day_03() {
                 if check(
                     s1.start.0, s1.start.1, e1.0, e1.1, s2.start.0, s2.start.1, e2.0,
                 ) {
-                    Some((s1.start.0, s2.start.1))
+                    Some((
+                        s1.start.0,
+                        s2.start.1,
+                        s1.clen + s2.clen + check2(s1, s2, s1.start.0, s2.start.1),
+                    ))
                 } else {
                     None
                 }
@@ -161,7 +180,11 @@ fn day_03() {
                 if check(
                     s2.start.0, s2.start.1, e2.0, e2.1, s1.start.0, s1.start.1, e1.0,
                 ) {
-                    Some((s2.start.0, s1.start.1))
+                    Some((
+                        s2.start.0,
+                        s1.start.1,
+                        s1.clen + s2.clen + check2(s2, s1, s2.start.0, s1.start.1),
+                    ))
                 } else {
                     None
                 }
@@ -175,6 +198,7 @@ fn day_03() {
         .map_while(Result::ok)
         .map(|l| {
             let mut start = (0, 0);
+            let mut clen = 0;
             l.trim()
                 .split(",")
                 .map(|x| {
@@ -195,7 +219,9 @@ fn day_03() {
                         start,
                         dir: d,
                         len: l,
+                        clen,
                     };
+                    clen += l;
                     start = match d {
                         Dir::U => (start.0, start.1 + l),
                         Dir::D => (start.0, start.1 - l),
@@ -207,18 +233,23 @@ fn day_03() {
                 .collect()
         })
         .collect();
-    let intersection: i64 = circuits[0]
+    let intersections: Vec<(i64, i64, i64)> = circuits[0]
         .iter()
         .flat_map(|s1| {
             circuits[1]
                 .iter()
                 .filter_map(|s2| intersect(s1, s2))
-                .collect::<Vec<(i64, i64)>>()
+                .filter(|(x, y, _)| !(*x == 0 && *y == 0))
+                .collect::<Vec<(i64, i64, i64)>>()
         })
-        .map(|(x, y)| x.abs() + y.abs())
+        .collect();
+    let res1 = intersections
+        .iter()
+        .map(|(x, y, _)| x.abs() + y.abs())
         .min()
         .unwrap();
-    println!("{:?}", intersection);
+    let res2 = intersections.iter().map(|x| x.2).min().unwrap();
+    println!("{res1} {res2}");
 }
 
 fn main() {
